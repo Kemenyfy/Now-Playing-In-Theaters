212202502802fd64d22ce68496440c2db52d808b
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-30 19:35:34 -0400

    Movies and Headers are now displaying.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import './App.css';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    }
+  }
+
+
   componentDidMount() {
     fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=9fe2614f2b9d0f56b7e758ac2b8ef828")
       .then(resp => resp.json())
       .then(json => {
-        console.log(json)
-      })
+        console.log(json.results)
+        this.setState({
+          movies: json.results
+      });
+    })
   }
 
   render() {
@@ -17,8 +28,14 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Now Playing in a Theater Near You</h1>
         </header>
-        <p>hjfkyxckj
-        </p>
+        {this.state.movies.map((movie, i) => {
+          return (
+            <section className="nowPlaying" key={i}>
+              <header>{movie.original_title}</header>
+              <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="Movie Poster"/>
+            </section>
+          );
+        })}
       </div>
     );
   }
